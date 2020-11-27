ddcc7bde2ad2a08f71e08fd920fea560dbdfd13e
MilosRadin <milosradin86@gmail.com>
2020-11-27 15:51:24 +0100

    Navigation item and DrawerListAdapter are finished

---
+++ app/src/main/java/com/ftninformatika/zadatak10/adapters/DrawerListAdapter.java
@@ -0,0 +1,57 @@
+package com.ftninformatika.zadatak10.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ftninformatika.zadatak10.R;
+import com.ftninformatika.zadatak10.model.NavigationItem;
+
+import java.util.List;
+
+public class DrawerListAdapter extends BaseAdapter {
+
+    private List<NavigationItem> items = null;
+    private Activity activity;
+
+    public DrawerListAdapter(List<NavigationItem> items, Activity activity) {
+        this.items = items;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public NavigationItem getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = activity.getLayoutInflater().inflate(R.layout.drawer_single_list_item, null);
+        }
+
+        ImageView imageIcon = convertView.findViewById(R.id.imageView_Icon);
+        TextView tvTitle = convertView.findViewById(R.id.textView_Title_Drawer);
+        TextView tvSubTitle = convertView.findViewById(R.id.textView_Subtitle_Drawer);
+
+        tvTitle.setText(items.get(position).getTitle());
+        tvSubTitle.setText(items.get(position).getSubtitle());
+
+        imageIcon.setImageResource(items.get(position).getIcon());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/ftninformatika/zadatak10/fragments/DetailsFragment.java
+++ app/src/main/java/com/ftninformatika/zadatak10/fragments/DetailsFragment.java
@@ -20,7 +20,6 @@ public class DetailsFragment extends Fragment {
     private Movie movie;
 
     public DetailsFragment() {
-        // Required empty public constructor
     }
 
 
@@ -31,7 +30,7 @@ public class DetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_details, container, false);
 
         setHasOptionsMenu(true);
